f3c8b7cb7678fc079f485da1efa2ad16dcb66553
Miroslav Stancic <miroslav.stancic@jsguru.io>
2016-09-19 21:08:04 +0200

    Adding order by feature to phone list - example of two way binding

--- app.module.js
+++ app.module.js
@@ -1,5 +1,5 @@
 'use strict'
 
 // Define the 'phonecatApp' module
-var phonecatApp = angular.module('phonecatApp', [ // which depends on 'phoneList' module 
+angular.module('phonecatApp', [ // which depends on 'phoneList' module 
 'phoneList']);
